7f95022ba776505d4cc53191d625d55f4c4e1fa5
Lucas Schlestein <lschlestein@gmail.com>
2024-08-14 15:56:52 -0300

    UPDATE to a gateway

--- src/main/java/client/oauthclient/OauthClientApplication.java
+++ src/main/java/client/oauthclient/OauthClientApplication.java
@@ -2,9 +2,8 @@ package client.oauthclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.Map;
@@ -14,14 +13,5 @@ public class OauthClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OauthClientApplication.class, args);
-    }
-
-    @Controller
-    @ResponseBody
-    class GreetingController {
-        @GetMapping("/hello")
-        public Map<String, String> hello(Principal principal) {
-            return Map.of("message", "Hello" + principal.getName() + "!");
-        }
     }
 }
